df88ad4e5a3dc66f052128a5e6a6ebf3bc83ae9c
Björn Gustavsson <bgustavsson@gmail.com>
2015-09-02 07:34:18 +0200

    Wings3D.sh: Sanitize the environment before starting Wings
    On an computer where Wings development had been done, the
    ERL_LIBS environment variable would probably be set. If the
    released version of Wings was run on such a computer, the
    result could be a crash or that a different version of Wings
    was run.
    
    Other environment variables recognized by the Erlang run-time
    system could potentially also cause problems. Therefore we will
    unset all environment variables that start with "ERL_".

--- macosx/Wings3D.sh
+++ macosx/Wings3D.sh
@@ -8,4 +8,11 @@ export EMU
 export ROOTDIR
 export BINDIR
 export PROGNAME
+# Get rid of environment variables starting with ERL_ which may
+# cause Wings to misbehave (ERL_LIBS in particular).
+for e in $(env); do
+  case $e in
+    ERL_*) unset ${e%%=*} ;;
+  esac
+done
 exec "$BINDIR/erlexec" -smp -noshell -run wings_start start_halt ${1+"$@"}
